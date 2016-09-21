4712da389fd4769e4caa3016e310cabecd7325d3
Clifton Regis <github email address>
2016-09-21 14:21:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = "I don't do much."
+
+function Shape(sides, x, y){
+	this.sides = sides;
+	this.x = x;
+	this.y = y;
+}
+function Quadrilateral(x,y, oneLength, twoLength, threeLength, fourLength){
+	Shape.call(this, 4, x, y);  //call on the superclass
+	this.oneLength = oneLength;
+	this.twoLength = twoLength;
+	this.threeLength = threeLength;
+	this.fourLength = fourLength;
+}
+
+Quadrilateral.prototype = Object.create(Shape.prototype)
+Quadrilateral.prototype.constructor = Quadrilateral;
+
+Quadrilateral.prototype.perimeter = function() {
+return this.oneLength + this.twoLength + this.threeLength + this.fourLength
+}
+
+};
